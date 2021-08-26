dc89d190f4ed471a376f33b5dfee980547a48eda
Rohit Vishwakarma <69453375+rohitec01@users.noreply.github.com>
2021-08-26 22:45:11 +0530

    initial cmmt

---
+++ exptrack/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate'
+import Card from './Card';
+const ExpenseItem = (props) => {
+    
+    return(
+        // <h2>Expense Item</h2>
+        <Card className="expense-item" >
+            {/* <div>{props.date.toISOString()}</div>  */}
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price" >${props.amount}</div>
+            </div>
+        </Card>        
+    )
+}
+
+export default ExpenseItem;
