14ee1f351eb7e08aead5ec2d14e9c0cceb810d35
Rohit Vishwakarma <69453375+rohitvish12@users.noreply.github.com>
2021-10-06 12:45:09 +0530

    Update Routes.js

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-// import Transaction from '../modal/Transaction.js'
 import {addTransactions, getTransactions} from '../controllers/Transaction.js';
 const router=express.Router();
 
@@ -10,17 +9,3 @@ router.get('/transaction', getTransactions);
 
 export default router;
 
-// async(req, res) => {
-//     // res.send(`Hello server from router js`);
-//     try {
-//         // console.log(req.body);
-//         // res.json({massege:req.body});
-//         // const Expense = req.body;
-//         // console.log(Expense);
-//         // const NewTransaction = new Transaction(Expense);
-//         // await NewTransaction.save();
-//         // res.json({message: 'Your Expenses is saved on mongo!'})
-        
-//     } catch (error) {
-//         console.log(error.message);
-//     }
